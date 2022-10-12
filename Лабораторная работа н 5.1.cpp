c7830a1354080d3ac2938a9af79c3154f19aa0ef
k1ruuu <kizz63@mail.ru>
2022-10-12 20:06:07 +0200

    Create Лабораторная работа н 5.1.cpp

---
+++ Лабораторная работа н 5.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char str[100] = "iuewrbhgpierwufgsiyegfTRYOUAggfysea";
+	char vol[7] = "aeioyu";
+	for (int i = 0; i < strlen(str); i++) {
+		for (int j = 0;j < strlen(vol);j++)
+			if (str[i] == vol[j]) {
+				str[i] -= 32;
+			}
+	}
+	cout << str;
+}
